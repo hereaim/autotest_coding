67382f7c118c953ab804a10f20765a1f4a937c04
Alexander <37065796+hereaim@users.noreply.github.com>
2024-06-11 20:31:23 +0700

    Update config.py

--- coding/ParserJira_Qase/config.py
+++ coding/ParserJira_Qase/config.py
@@ -1,4 +1,4 @@
-token = '5dad3438e15f52f629379ce51ca4d16504e27ef9c58a01b99865a8f6dda934dd'
+token = ''
 base_url = "https://api.qase.io/v1/case/" #Базовый URL для добавления Тест-Кейсов
 project_in_qase = "TFJ" #Наименование проекта в qase(берется из ссылки)
 base_url_case = base_url+project_in_qase
