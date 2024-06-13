a24b28d994281ac8e5855e1c168895c922089921
Alexander <37065796+hereaim@users.noreply.github.com>
2024-06-13 16:52:20 +0700

    Update config.py

--- coding/ParserJira_Qase/config.py
+++ coding/ParserJira_Qase/config.py
@@ -1,4 +1,4 @@
-token = ''
+token = '' #API токен Qase
 base_url = "https://api.qase.io/v1/case/" #Базовый URL для добавления Тест-Кейсов
 project_in_qase = "" #Наименование проекта в qase(берется из ссылки)
 base_url_case = base_url+project_in_qase
